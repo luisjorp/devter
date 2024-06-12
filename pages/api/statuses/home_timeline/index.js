d82bb5e37fdd170be3df06808b0ab4acade52f70
Luisjo <luiso@MacBook-Pro-de-Luisjo.local>
2024-06-12 16:12:00 -0600

    UPD: hardcoded timeline expanded to 20 tweets

--- pages/api/statuses/home_timeline/index.js
+++ pages/api/statuses/home_timeline/index.js
@@ -46,6 +46,137 @@ export default function handler(req, res) {
         avatar:
           "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
       },
+      {
+        id: "6",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "Debugging is like being the detective in a crime movie where you are also the murderer.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "7",
+        username: "luisjo",
+        name: "Luis Jo",
+        message: "Why do Java developers wear glasses? Because they don’t C#.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "8",
+        username: "luisjo",
+        name: "Luis Jo",
+        message: "In case of fire: git commit, git push, leave building.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "9",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "To understand recursion, one must first understand recursion.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "10",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "There are only two hard things in Computer Science: cache invalidation and naming things.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "11",
+        username: "luisjo",
+        name: "Luis Jo",
+        message: "Programmer: A machine that turns coffee into code.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "12",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "99 little bugs in the code, 99 bugs in the code, take one down, patch it around, 127 little bugs in the code.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "13",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "I used to think the brain was the most important organ. Then I realized what was telling me that.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "14",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "Software and cathedrals are much the same – first we build them, then we pray.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "15",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "I don’t always test my code, but when I do, I do it in production.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "16",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "The best thing about a boolean is even if you are wrong, you are only off by a bit.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "17",
+        username: "luisjo",
+        name: "Luis Jo",
+        message: "“It works on my machine” doesn’t count as a fix.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "18",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "A user interface is like a joke. If you have to explain it, it’s not that good.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "19",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "Algorithm: Word used by programmers when they don’t want to explain what they did.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
+      {
+        id: "20",
+        username: "luisjo",
+        name: "Luis Jo",
+        message:
+          "I would love to change the world, but they won’t give me the source code.",
+        avatar:
+          "https://avatars.githubusercontent.com/u/67595890?s=400&u=fb2c3c0e4d6846e73bfdbbde76c52ed06f14c9ef&v=4",
+      },
     ]),
   )
 }
