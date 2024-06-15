5a373bcb0185578b32452c6085487441153b788d
LuisJo <rp.luisjose@gmail.com>
2024-06-14 21:29:46 -0600

    ADD: useTimeAgo Hook to show the timeAgo in devits

--- hooks/useTimeAgo.js
+++ hooks/useTimeAgo.js
@@ -1,3 +1,5 @@
+import { useEffect, useState, useCallback } from "react"
+
 const DATE_UNITS = [
   ["day", 86400],
   ["hour", 3600],
@@ -18,11 +20,24 @@ const getDateDiffs = (timestamp) => {
 }
 
 export default function useTimeAgo(timestamp) {
-  const { value, unit } = getDateDiffs(timestamp)
+  const [timeAgo, setTimeAgo] = useState(() => getDateDiffs(timestamp))
 
   const rtf = new Intl.RelativeTimeFormat("es", {
     style: "short",
   })
+
+  // Memoize the function that updates the timeAgo state
+  const updateTimeAgo = useCallback(() => {
+    const newTimeAgo = getDateDiffs(timestamp)
+    setTimeAgo(newTimeAgo)
+  }, [timestamp])
+
+  useEffect(() => {
+    const interval = setInterval(updateTimeAgo, 5000)
+    return () => clearInterval(interval)
+  }, [updateTimeAgo])
+
+  const { value, unit } = timeAgo
 
   return rtf.format(value, unit)
 }
