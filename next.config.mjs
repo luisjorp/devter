a74063b43ccd716fbcf615cfa99b5502206e9ebf
LuisJo <rp.luisjose@gmail.com>
2024-06-15 16:01:47 -0600

    ADD: plaiceholder to blur images loading in homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPlaiceholder from "@plaiceholder/next";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -25,4 +27,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPlaiceholder(nextConfig);
