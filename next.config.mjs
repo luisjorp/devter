67ebaffbf9b0ee56c9ca6a32429798d9bce73ebb
LuisJo <rp.luisjose@gmail.com>
2024-06-14 21:31:41 -0600

    ADD: Drag and Drop images, firebase storage

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'pbs.twimg.com',
         port: '',
+      },
+      {
+        // This pattern is used to load images from the Firebase Storage CDN
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
       }
     ],
   },
